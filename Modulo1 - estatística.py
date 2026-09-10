0e116e19d4f42b53195175b345df4110c11edc99
lucas xavier <lucas.xavier@sempreceub.com>
2026-09-10 11:25:35 -0300

    Remove arquivos soltos: rascunho incompleto de estatistica e copia de teste do Modulo 0 com caminho pessoal (nao usados em nada do projeto)

--- Modulo1 - estatística.py
+++
@@ -1,7 +0,0 @@
-# arquivo: minhastats.py
-def media(lista):
-    return sum(lista) / len(lista)
-
-def variancia_amostral(lista):
-    m = media(lista)
-    return sum((x - m) ** 2 for x in lista) / (len(lista) - 1)
